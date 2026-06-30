af8b0a8de6297fee3c68bc02350211934864339c
Samrat Talukdar <69191476+ogsamrat@users.noreply.github.com>
2026-07-01 02:40:41 +0530

    style(web): card top sheen

--- web/src/components/ui/card.tsx
+++ web/src/components/ui/card.tsx
@@ -2,7 +2,9 @@ import * as React from "react";
 import { cn } from "@/lib/utils";
 
 export function Card({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return <div className={cn("rounded-lg border border-hairline bg-surface", className)} {...props} />;
+  return (
+    <div className={cn("sheen rounded-lg border border-hairline bg-surface", className)} {...props} />
+  );
 }
 
 export function CardHeader({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {

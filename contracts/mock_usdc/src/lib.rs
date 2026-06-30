cd51e863346c001e5f6ea1ad8ae2477943fc454e
Samrat Talukdar <69191476+ogsamrat@users.noreply.github.com>
2026-07-01 02:13:14 +0530

    feat(mock-usdc): implement lib

--- contracts/mock_usdc/src/lib.rs
+++ contracts/mock_usdc/src/lib.rs
@@ -53,3 +53,59 @@ impl MockUsdc {
         }
         Base::mint(e, &to, amount);
     }
+}
+
+#[contractimpl]
+impl FungibleToken for MockUsdc {
+    type ContractType = Base;
+
+    fn total_supply(e: &Env) -> i128 {
+        Self::ContractType::total_supply(e)
+    }
+
+    fn balance(e: &Env, account: Address) -> i128 {
+        Self::ContractType::balance(e, &account)
+    }
+
+    fn allowance(e: &Env, owner: Address, spender: Address) -> i128 {
+        Self::ContractType::allowance(e, &owner, &spender)
+    }
+
+    fn transfer(e: &Env, from: Address, to: MuxedAddress, amount: i128) {
+        Self::ContractType::transfer(e, &from, &to, amount);
+    }
+
+    fn transfer_from(e: &Env, spender: Address, from: Address, to: Address, amount: i128) {
+        Self::ContractType::transfer_from(e, &spender, &from, &to, amount);
+    }
+
+    fn approve(e: &Env, owner: Address, spender: Address, amount: i128, live_until_ledger: u32) {
+        Self::ContractType::approve(e, &owner, &spender, amount, live_until_ledger);
+    }
+
+    fn decimals(e: &Env) -> u32 {
+        Self::ContractType::decimals(e)
+    }
+
+    fn name(e: &Env) -> String {
+        Self::ContractType::name(e)
+    }
+
+    fn symbol(e: &Env) -> String {
+        Self::ContractType::symbol(e)
+    }
+}
+
+#[contractimpl]
+impl FungibleBurnable for MockUsdc {
+    fn burn(e: &Env, from: Address, amount: i128) {
+        Self::ContractType::burn(e, &from, amount)
+    }
+
+    fn burn_from(e: &Env, spender: Address, from: Address, amount: i128) {
+        Self::ContractType::burn_from(e, &spender, &from, amount)
+    }
+}
+
+#[cfg(test)]
+mod test;

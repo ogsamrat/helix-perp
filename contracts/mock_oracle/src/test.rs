4b5acfe6bd78f9adb896befd885ae20c1b814153
Samrat Talukdar <69191476+ogsamrat@users.noreply.github.com>
2026-07-01 02:13:19 +0530

    test(mock-oracle): implement test

--- contracts/mock_oracle/src/test.rs
+++ contracts/mock_oracle/src/test.rs
@@ -15,3 +15,21 @@ fn set_and_read_price() {
     e.mock_all_auths();
     let oracle = setup(&e);
     let xau = ReflectorAsset::Other(Symbol::new(&e, "XAU"));
+
+    assert_eq!(oracle.decimals(), 14);
+    assert!(oracle.lastprice(&xau).is_none());
+
+    oracle.set_price(&xau, &(2_400i128 * 100_000_000_000_000)); // $2400 @ 14dp
+    let pd = oracle.lastprice(&xau).unwrap();
+    assert_eq!(pd.price, 2_400i128 * 100_000_000_000_000);
+}
+
+#[test]
+fn explicit_timestamp_for_stale_simulation() {
+    let e = Env::default();
+    e.mock_all_auths();
+    let oracle = setup(&e);
+    let eur = ReflectorAsset::Other(Symbol::new(&e, "EUR"));
+    oracle.set_price_at(&eur, &(108i128 * 1_000_000_000_000), &1000u64);
+    assert_eq!(oracle.lastprice(&eur).unwrap().timestamp, 1000);
+}
